b96338640343d01f42096cacc805e9ec3de60676
Todd W Crone <twcrone@gmail.com>
2021-11-20 16:24:16 -0500

    Make it kinda worky

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,21 @@
 use bracket_lib::prelude::*;
 
+const SCREEN_WIDTH : i32 = 80;
+const SCREEN_HEIGHT : i32 = 50;
+const FRAME_DURATION : f32 = 75.0;
+
 struct State {
+    frame_time: f32,
     mode: GameMode,
+    player: Player,
 }
 
 impl State {
     fn new() -> Self {
         State {
+            frame_time: 0.0,
             mode: GameMode::Menu,
+            player: Player::new(5, 25),
         }
     }
 
@@ -26,13 +34,27 @@ impl State {
         }
     }
 
-    fn play(&mut self, _ctx: &mut BTerm) {
-        // TODO: Fill in this stub later
-        self.mode = GameMode::End;
+    fn play(&mut self, ctx: &mut BTerm) {
+        ctx.cls_bg(NAVY);
+        self.frame_time += ctx.frame_time_ms;
+        if self.frame_time > FRAME_DURATION {
+            self.frame_time = 0.0;
+            self.player.gravity_and_move();
+        }
+        if let Some(VirtualKeyCode::Space) = ctx.key {
+            self.player.flap();
+        }
+        self.player.render(ctx);
+        ctx.print(0, 0, "Press SPACE to flap.");
+        if self.player.y > SCREEN_HEIGHT {
+            self.mode = GameMode::End;
+        }
     }
 
     fn restart(&mut self) {
+        self.frame_time = 0.0;
         self.mode = GameMode::Playing;
+        self.player = Player::new(5, 25);
     }
 
     fn dead(&mut self, ctx: &mut BTerm) {
@@ -65,6 +87,47 @@ enum GameMode {
     Menu,
     Playing,
     End,
+}
+
+struct Player {
+    x: i32,
+    y: i32,
+    velocity: f32,
+}
+
+impl Player {
+    fn new(x: i32, y: i32) -> Self {
+        Player {
+            x,
+            y,
+            velocity: 0.0,
+        }
+    }
+
+    fn render(&mut self, ctx: &mut BTerm) {
+        ctx.set(
+            0,
+            self.y,
+            YELLOW,
+            BLACK,
+            to_cp437('@')
+        );
+    }
+
+    fn gravity_and_move(&mut self) {
+        if self.velocity < 2.0 {
+            self.velocity += 0.2;
+        }
+        self.y += self.velocity as i32;
+        self.x += 1;
+        if self.y < 0 {
+            self.y = 0;
+        }
+    }
+
+    fn flap(&mut self) {
+        self.velocity = -2.0;
+    }
 }
 
 fn main() -> BError {
